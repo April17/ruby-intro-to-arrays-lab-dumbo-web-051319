2cf944cd40f71b45ee19bfe9fa49a6377351aff1
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-24 23:58:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,7 +21,7 @@ def first_element_with_array_methods(array)
   return array.first
 end
 
-edf last_element_with_array_methods(array)
+def last_element_with_array_method (array)
   return array.last
 end
 
